677a26eb078c9c6a4a5bae76dd055de21d565fac
bellaisafranco <isabella.franco@edu.sait.ca>
2024-10-31 16:38:19 -0600

    newq bttn

--- intro.js
+++ intro.js
@@ -1,5 +1,17 @@
+//pop up secret message
 swag.addEventListener('click', function(){
     alert("boi oi oi oing!");
-
+}
+)
+//make light bulb image change when user clicks button
+light_on.addEventListener('click', function(){
+console.log("let there be swag");
+document.getElementById("lightbulb").src="lightbulb_on.webp";
+}
+)
+//make light bulb image change when user clicks button
+light_off.addEventListener('click', function(){
+console.log("too far");
+document.getElementById("lightbulb").src="lightbulb_off.webp";
 }
 )
